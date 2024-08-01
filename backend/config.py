c010444738c2342822959372acefb5e2e5ccdec7
Subeen Regmi <subeenregmi123@gmail.com>
2024-08-01 14:22:37 +0100

    added config file tests and github workflow

--- backend/config.py
+++ backend/config.py
@@ -15,7 +15,20 @@ def getConfig(filename: str) -> dict[str, str]:
     with open(filename, "r") as f:
         data = yaml.load(f, yaml.Loader)
 
-    # If secret_key is not in the yaml file
+    data = checkSecret(data)
+
+    return data
+
+
+def checkSecret(data: dict[str, str]):
+    """ Checks if secret is present in data, if not it will generate it
+
+    Args:
+        data: The current yaml file in dictionary value
+
+    Returns:
+        The 'data' dictionary with the secret_key
+    """
     if "secret_key" not in data or data["secret_key"] is None:
         data["secret_key"] = secrets.token_hex()
 
---
+++ tests/config_test.py
@@ -0,0 +1,32 @@
+import pytest
+import sys
+sys.path.insert(0, "..")
+import backend.config as config
+
+
+class TestConfig:
+    file1 = config.getConfig("test_config_one.yaml")
+    file2 = config.getConfig("test_config_two.yaml")
+
+    def test_load_one(self):
+        assert self.file1["database"] == "onefile.db"
+
+    def test_load_two(self):
+        assert self.file1["loginRequired"]
+
+    def test_load_three(self):
+        secret = (
+            "7e6dac2a942ba1d025171d1d7d9b9719e003a6c85f599d8a1b0c2fa7d8899af1"
+        )
+        assert self.file1["secret_key"] == secret
+
+    def test_load_four(self):
+        assert len(self.file1) == 3
+
+    def test_secret_generation_one(self):
+        assert self.file2["secret_key"]
+
+    def test_secret_generation_two(self):
+        value = self.file1.copy()
+        config.checkSecret(value)
+        assert value["secret_key"] == self.file1["secret_key"]
