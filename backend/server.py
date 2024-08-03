2d7fda62f9c05e816b2834d331860f435902906a
Subeen Regmi <subeenregmi123@gmail.com>
2024-08-03 13:51:14 +0100

    add

--- backend/server.py
+++ backend/server.py
@@ -12,6 +12,42 @@ from database_utils import (
 )
 from bcrypt import hashpw, checkpw, gensalt
 from config import getConfig
+from logging.config import dictConfig
+
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format":
+                "[%(asctime)s] | %(levelname)s %(funcName)s | %(message)s",
+                "datefmt": "%H:%M - %d-%m-%Y ",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default"
+            },
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": "server.log",
+                "maxBytes": 5000000,
+                "backupCount": 5,
+                "formatter": "default"
+            },
+        },
+        "root": {
+            "level": "DEBUG",
+            "handlers": [
+                "console",
+                "file"
+            ]
+        },
+    }
+)
 
 
 config = getConfig("config.yaml")
@@ -31,6 +67,7 @@ def home():
     if config['loginRequired']:
         return render_template("homepage.html")
     else:
+        app.logger.info("Anonymous user logged in.")
         session['username'] = "Anonymous"
         session['privilege'] = "3"
         session['user_id'] = "-1"
@@ -53,6 +90,7 @@ def login():
     user = user[0]
 
     if not checkpw(password, user['PassHash'].encode("UTF-8")):
+        app.logger.info(f"User '{username}' login failed.")
         return render_template("homepage.html")
 
     session["username"] = user['Username']
@@ -71,16 +109,27 @@ def dashboard():
 
     # Cannot login with the anonymous account if login is needed
     if username == "Anonymous" and config["loginRequired"]:
+        app.logger.warning(
+            "Anonymous passed login but failed dashboard check."
+        )
         return redirect(url_for('home'))
 
     match privilege:
         case "1":
+            app.logger.info(f"Admin '{username}' has successfully logged in.")
             return render_template("adminpage.html")
         case "2":
+            app.logger.info(
+                f"Uploader '{username}' has successfully logged in."
+            )
             return render_template("uploadpage.html")
         case "3":
+            app.logger.info(f"Viewer '{username}' has successfully logged in.")
             return render_template("loginpage.html", username=username)
         case _:
+            app.logger.warning(
+                f"User '{username}' has tried to login with unknown privilege."
+            )
             return redirect(url_for('home'))
 
 
@@ -90,9 +139,13 @@ def uploadFile():
     form data.
     """
 
+    username = session.get("username")
     privilege = session.get("privilege")
 
     if (privilege != "1" and privilege != "2"):
+        app.logger.warning(
+            f"User '{username}' has tried to upload a file without the correct"
+            " permissions.")
         return "You need to be an admin or an uploader to upload files"
 
     filename = request.form["filename"]
@@ -100,6 +153,7 @@ def uploadFile():
     file.save(f"shared_files/{filename}")
     addFile(db_conn, filename)
 
+    app.logger.info(f"User {username} has added a new file: '{filename}'.")
     return "The file has been successfully added"
 
 
@@ -109,14 +163,20 @@ def deleteFile():
     form data.
     """
 
+    username = session.get("username")
     privilege = session.get("privilege")
 
     if (privilege != "1" and privilege != "2"):
+        app.logger.warning(
+            f"User '{username}' has tried to delete a file without the correct"
+            " permissions.")
         return "You need to be an admin or an uploader to upload files"
 
     filename = request.form["filename"]
     removeFileHistory(db_conn, filename)
     removeFile(db_conn, filename)
+
+    app.logger.info(f"User {username} has delete a new file: '{filename}'.")
     return "The file has been successfully deleted"
 
 
@@ -128,6 +188,9 @@ def getFile(filename):
     if filename == "all":
         filename = ""
 
+    app.logger.debug(
+        f"File information for '{filename or "*"}' is being retrieved."
+    )
     return getFileData(db_conn, filename, *columns)
 
 
@@ -141,17 +204,23 @@ def downloadFile(filename: str):
     response.headers["Content-Disposition"] = "attachment"
 
     file = getFileData(db_conn, filename)
+    username = session.get("username")
     userID = session.get("user_id")
 
     addDownloadTransaction(db_conn, userID, file[0]['ID'])
     incrementDownloadCount(db_conn, filename)
 
+    app.logger.info(f"User {username} has downloaded {filename}.")
     return response
 
 
 @app.route("/api/file/stats", methods=["POST"])
 def getFileStats():
     """ Api route to retrieve file download history.  """
+
+    app.logger.debug(
+        f"File statistics for '{request.form['filename']}' retrieved."
+    )
     return getFileStatistics(db_conn, request.form['filename'])
 
 
@@ -159,8 +228,13 @@ def getFileStats():
 def addUser():
     """ Api route to create a new user, admins can only create new users """
     privilege = session.get("privilege")
+    username = session.get("username")
 
     if (privilege != "1"):
+        app.logger.warning(
+            f"User '{username}' has tried to add a new user without the"
+            " correct permissions."
+        )
         return "You need to be an admin to create new users"
 
     newUsername = request.form["username"]
@@ -169,6 +243,9 @@ def addUser():
     newPrivilege = request.form["privilege"]
 
     createUser(db_conn, newUsername, newPassHash, newPrivilege)
+    app.logger.info(
+        f"New user '{newUsername}' has been created by '{username}'."
+    )
     return "The new user has been created"
 
 
@@ -177,12 +254,19 @@ def deleteUser():
     """ Api route to delete a user from the database """
 
     privilege = session.get("privilege")
+    usern = session.get("username")
 
     if (privilege != "1"):
+        app.logger.warning(
+            f"User '{usern}' has tried to delete a user without the"
+            " correct permissions."
+        )
         return "You need to be admin to remove new users"
 
     username = request.form["username"]
     removeUser(db_conn, username)
+
+    app.logger.info(f"User '{username}' has been deleted by user '{usern}'.")
     return "The user has been deleted"
 
 
@@ -194,13 +278,21 @@ def getUser(username: str):
 
     columns = request.args.to_dict(flat=False)['cols']
 
+    app.logger.debug(f"User data for '{username or "*"}' has been retrieved.")
     return getUserData(db_conn, username, "", *columns)
 
 
-if __name__ == "__main__":
+def main():
     app.debug = True
 
     if not config['loginRequired']:
+        app.logger.info("Anonymous user has been created.")
         createAnonUser(db_conn)
 
+    app.logger.info("="*50)
+    app.logger.info("Starting app...")
     app.run()
+
+
+if __name__ == "__main__":
+    main()
